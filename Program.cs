31f72dce2b22a502a74e8be94cbd2aa196c60008
flip701 <paulshipspb@gmail.com>
2023-04-13 15:26:20 +0300

    Решение второй задачи

--- Program.cs
+++ Program.cs
@@ -3,21 +3,48 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
+// Console.Write("Введите первое число: ");
+// int num1 = int.Parse(Console.ReadLine());
+
+// Console.Write("Введите вторе число: ");
+// int num2 = int.Parse(Console.ReadLine());
+
+// if (num1 > num2)
+// {
+//     Console.WriteLine($"{num1} больше, чем {num2}");
+// }
+// else if (num2 > num1)
+// {
+//     Console.WriteLine($"{num2} больше, чем {num1}");
+// }
+// else
+// {
+//     Console.WriteLine($"{num1} и {num2} равны");
+// }
+
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Console.WriteLine("Введите три числа:");
 Console.Write("Введите первое число: ");
 int num1 = int.Parse(Console.ReadLine());
-
-Console.Write("Введите вторе число: ");
+Console.Write("Введите второе число: ");
 int num2 = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = int.Parse(Console.ReadLine());
 
-if (num1 > num2)
+int max = num1;
+
+if (num2 > max)
 {
-    Console.WriteLine($"{num1} больше, чем {num2}");
+max = num2;
 }
-else if (num2 > num1)
+
+if (num3 > max)
 {
-    Console.WriteLine($"{num2} больше, чем {num1}");
+max = num3;
 }
-else
-{
-    Console.WriteLine($"{num1} и {num2} равны");
-}
+Console.WriteLine($"Максимальное число: {max}");
+        
